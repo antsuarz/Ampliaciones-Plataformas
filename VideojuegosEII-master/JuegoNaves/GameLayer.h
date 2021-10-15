32d43d40dde9656325dda443fd0f7a6e90254cd9
ANTONIO SUAREZ CRESPO <UO270543@uniovi.es>
2021-10-15 20:09:35 +0200

    1st commit

---
+++ VideojuegosEII-master/JuegoNaves/GameLayer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "Layer.h"
+#include "Player.h"
+#include "Audio.h" 
+
+#include "Background.h"
+#include "Enemy.h"
+#include "Projectile.h"
+#include "Text.h"
+#include "Tile.h"
+#include "Space.h"
+#include <list>
+
+#include "Pad.h"
+#include <fstream> // Leer ficheros
+#include <sstream> // Leer líneas / String
+
+
+
+class GameLayer : public Layer
+{
+public:
+	GameLayer(Game* game);
+	void init() override;
+	void processControls() override;
+	void update() override;
+	void draw() override;
+	void keysToControls(SDL_Event event);
+	int newEnemyTime = 0;
+	void loadMap(string name);
+	void loadMapObject(char character, float x, float y);
+	int mapWidth;
+	list<Tile*> tiles;
+	void calculateScroll();
+	float scrollX;
+	Space* space;
+	Audio* audioBackground;
+	Text* textPoints;
+	Text* textLifes;
+	int points;
+	int lifes;
+	Player* player;
+	Background* background;
+	Actor* backgroundPoints;
+	Actor* backgroundLifes;
+	bool controlShoot = false;
+	int controlMoveY = 0;
+	int controlMoveX = 0;
+	Tile* cup;
+	//Movidas pal ratón
+	// Elementos de interfaz
+	Pad* pad;
+	void mouseToControls(SDL_Event event); // USO DE MOUSE
+	Actor* buttonJump;
+	Actor* buttonShoot;
+	//---------------
+
+	list<Enemy*> enemies;
+	list<Projectile*> projectiles;
+
+};
+
---
+++ VideojuegosEII-master/JuegoNaves/Player.cpp
@@ -0,0 +1,155 @@
+#include "Player.h"
+
+Player::Player(float x, float y, Game* game)
+	: Actor("res/jugador.png", x, y, 35, 35, game) {
+
+	audioShoot = new Audio("res/efecto_disparo.wav", false);
+
+	aShootingRight = new Animation("res/jugador_disparando_derecha.png",
+		width, height, 160, 40, 6, 4, false, game);
+	aShootingLeft = new Animation("res/jugador_disparando_izquierda.png",
+		width, height, 160, 40, 6, 4, false, game);
+
+	aJumpingRight = new Animation("res/jugador_saltando_derecha.png",
+		width, height, 160, 40, 6, 4, false, game);
+	aJumpingLeft = new Animation("res/jugador_saltando_izquierda.png",
+		width, height, 160, 40, 6, 4, false, game);
+
+	aIdleRight = new Animation("res/jugador_idle_derecha.png", width, height,
+		320, 40, 6, 8, true, game);
+	aIdleLeft = new Animation("res/jugador_idle_izquierda.png", width, height,
+		320, 40, 6, 8, true, game);
+	aRunningRight = new Animation("res/jugador_corriendo_derecha.png", width, height,
+		320, 40, 6, 8, true, game);
+	aRunningLeft = new Animation("res/jugador_corriendo_izquierda.png", width, height,
+		320, 40, 6, 8, true, game);
+	animation = aIdleRight;
+	orientation = Orientation::RIGHT;
+}
+
+void Player::update() {
+	if (invulTime > 0) {
+		invulTime--;
+	}
+	if (vx > 0) {
+		orientation = Orientation::RIGHT;
+	}
+	if (vx < 0) {
+		orientation = Orientation::LEFT;
+	}
+	if (collisionDown == true) {
+		onAir = false;
+	}
+	else {
+		onAir = true;
+	}
+
+	if (onAir) {
+		state = States::JUMPING;
+	}
+	else {
+		state = States::MOVING;
+	}
+
+	bool hasAnimationEnded = animation->update();
+	if (hasAnimationEnded) {
+		// Estaba disparando
+		if (state == States::SHOOTING) {
+			state = States::MOVING;
+		}
+	}
+
+	// Selección de animación basada en estados
+	if (state == States::JUMPING) {
+		if (orientation == Orientation::RIGHT) {
+			animation = aJumpingRight;
+		}
+		if (orientation == Orientation::LEFT) {
+			animation = aJumpingLeft;
+		}
+	}
+	if (state == States::SHOOTING) {
+		if (orientation ==Orientation::RIGHT) {
+			animation = aShootingRight;
+		}
+		if (orientation == Orientation::LEFT) {
+			animation = aShootingLeft;
+		}
+	}
+	
+	if (state == States::MOVING || state == States::IDLE) {
+		if (vx != 0) {
+			if (orientation == Orientation::RIGHT) {
+				animation = aRunningRight;
+			}
+			if (orientation == Orientation::LEFT) {
+				animation = aRunningLeft;
+			}
+		}
+		if (vx == 0) {
+			if (orientation == Orientation::RIGHT) {
+				animation = aIdleRight;
+			}
+			if (orientation == Orientation::LEFT) {
+				animation = aIdleLeft;
+			}
+		}
+	}
+
+
+	if (shootTime > 0) {
+		shootTime--;
+	}
+}
+
+void Player::jump() {
+	if (!onAir) {
+		vy = -16;
+		onAir = true;
+	}
+
+}
+
+
+void Player::moveX(float axis) {
+	vx = axis * 3;
+}
+
+void Player::moveY(float axis) {
+	vy = axis * 3;
+}
+
+Projectile* Player::shoot() {
+	if (shootTime == 0) {
+		audioShoot->play();
+		state = States::SHOOTING;
+		shootTime = shootCadence;
+		auto proyectile = new Projectile(x, y, game);
+		if (orientation == Orientation::LEFT) {
+			proyectile->vx *= -1;
+		}
+		return proyectile;
+	}
+	else {
+		return NULL;
+	}
+
+}
+
+void Player::draw(float scrollX) {
+	if (invulTime == 0) {
+		animation->draw(x - scrollX, y);
+		return;
+	}
+	if(invulTime % 10 >= 0 && invulTime % 10 <= 5) {
+		animation->draw(x - scrollX, y);
+	}
+}
+
+void Player::loseLife() {
+	if (invulTime > 0 || lifes <= 0) {
+		return;
+	}
+	lifes--;
+	invulTime = 100;
+}
